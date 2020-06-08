0d9561b97089f1aba529c2f7406094866b38e9d7
Lexi Delorey <lexidelorey@gmail.com>
2020-06-08 19:27:29 +0000

    adding a map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -110,3 +110,14 @@ function deleteSingleComment(comment) {
     .then(getComments());
 }
 
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+function onLoad() {
+  createMap();
+  rotateImages();
+}
+
